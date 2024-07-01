7cc6b9fd6997f462454a1819ee37d0e4f15d1fd7
re-hiramatsu <re-hiramatsu>
2024-07-01 10:38:52 +0900

    カスタードとデラックスたい焼きのテスト実装

---
+++ TaiyakiSystemTest/ModelTest/CustardTaiyakiTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaiyakiSystem.Cores.Enums;
+using TaiyakiSystem.Cores.Models;
+
+namespace TaiyakiSystemTest.ModelTest
+{
+    [TestClass]
+    public class CustardTaiyakiTest
+    {
+        [TestMethod]
+        public void CustardTaiyakiConstructorTest()
+        {
+            var miniTaiyaki = new CustardTaiyaki(TaiyakiSizeEnum.小);
+            var middleTaiyaki = new CustardTaiyaki(TaiyakiSizeEnum.中);
+            var bigTaiyaki = new CustardTaiyaki(TaiyakiSizeEnum.大);
+
+            Assert.AreEqual(TaiyakiSizeEnum.小, miniTaiyaki.Size);
+            Assert.AreEqual(TaiyakiSizeEnum.中, middleTaiyaki.Size);
+            Assert.AreEqual(TaiyakiSizeEnum.大, bigTaiyaki.Size);
+
+            Assert.AreEqual(TaiyakiEnum.カスタードたい焼き, miniTaiyaki.Name);
+            Assert.AreEqual(TaiyakiEnum.カスタードたい焼き, middleTaiyaki.Name);
+            Assert.AreEqual(TaiyakiEnum.カスタードたい焼き, bigTaiyaki.Name);
+
+            Assert.AreEqual(TaiyakiContentEnum.カスタード, miniTaiyaki.Content);
+            Assert.AreEqual(TaiyakiContentEnum.カスタード, middleTaiyaki.Content);
+            Assert.AreEqual(TaiyakiContentEnum.カスタード, bigTaiyaki.Content);
+
+            Assert.AreEqual(150, miniTaiyaki.GetSubTotal());
+            Assert.AreEqual(200, middleTaiyaki.GetSubTotal());
+            Assert.AreEqual(250, bigTaiyaki.GetSubTotal());
+        }
+    }
+}
---
+++ TaiyakiSystemTest/ModelTest/DeluxeTaiyakiTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TaiyakiSystem.Cores.Enums;
+using TaiyakiSystem.Cores.Models;
+
+namespace TaiyakiSystemTest.ModelTest
+{
+    [TestClass]
+    public class DeluxeTaiyakiTest
+    {
+        [TestMethod]
+        public void DeluxeTaiyakiConstructorTest()
+        {
+            var bigTaiyaki = new DeluxeTaiyaki(TaiyakiSizeEnum.大);
+
+            Assert.ThrowsException<Exception>(() => new DeluxeTaiyaki(TaiyakiSizeEnum.小));
+            Assert.ThrowsException<Exception>(() => new DeluxeTaiyaki(TaiyakiSizeEnum.中));
+
+            Assert.AreEqual(TaiyakiSizeEnum.大, bigTaiyaki.Size);
+            Assert.AreEqual(TaiyakiEnum.デラックスたい焼き, bigTaiyaki.Name);
+            Assert.AreEqual(TaiyakiContentEnum.カスタード, bigTaiyaki.Content);
+            Assert.AreEqual(TaiyakiContentEnum.生クリーム, bigTaiyaki.AddingContent);
+            Assert.AreEqual(300, bigTaiyaki.GetSubTotal());
+        }
+    }
+}

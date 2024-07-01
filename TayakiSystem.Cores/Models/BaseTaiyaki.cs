d57955c1274ba417e76e55eb027cde17c249ca5a
re-hiramatsu <re-hiramatsu>
2024-07-01 10:39:28 +0900

    BaseTaiyakiの抽象クラス実装

---
+++ TayakiSystem.Cores/Models/BaseTaiyaki.cs
@@ -0,0 +1,20 @@
+﻿using TaiyakiSystem.Cores.Enums;
+namespace TaiyakiSystem.Cores.Models
+{
+    public abstract class BaseTaiyaki
+    {
+        public TaiyakiEnum Name { get; set; }
+        public TaiyakiSizeEnum Size { get; set; }
+        public TaiyakiContentEnum Content { get; set; }
+
+        public BaseTaiyaki(TaiyakiSizeEnum size)
+        {
+            Size = size;
+        }
+
+        public virtual int GetSubTotal()
+        {
+            return (int)Name + (int)Size;
+        }
+    }
+}

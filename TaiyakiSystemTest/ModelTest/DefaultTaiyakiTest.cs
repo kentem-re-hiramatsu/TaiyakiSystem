97ff53fff33e3dea121e816edcdaed3850d4f59c
re-hiramatsu <re-hiramatsu>
2024-07-01 10:38:09 +0900

    通常たい焼きテスト実装

---
+++ TaiyakiSystemTest/ModelTest/DefaultTaiyakiTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaiyakiSystem.Cores.Enums;
+using TaiyakiSystem.Cores.Models;
+
+namespace TaiyakiSystemTest.ModelTest
+{
+    [TestClass]
+    public class DefaultTaiyakiTest
+    {
+        [TestMethod]
+        public void DefaultTaiyakiConstructorTest()
+        {
+            var miniTaiyaki = new DefaultTaiyaki(TaiyakiSizeEnum.小);
+            var middleTaiyaki = new DefaultTaiyaki(TaiyakiSizeEnum.中);
+            var bigTaiyaki = new DefaultTaiyaki(TaiyakiSizeEnum.大);
+
+            Assert.AreEqual(TaiyakiSizeEnum.小, miniTaiyaki.Size);
+            Assert.AreEqual(TaiyakiSizeEnum.中, middleTaiyaki.Size);
+            Assert.AreEqual(TaiyakiSizeEnum.大, bigTaiyaki.Size);
+
+            Assert.AreEqual(TaiyakiEnum.通常たい焼き, miniTaiyaki.Name);
+            Assert.AreEqual(TaiyakiEnum.通常たい焼き, middleTaiyaki.Name);
+            Assert.AreEqual(TaiyakiEnum.通常たい焼き, bigTaiyaki.Name);
+
+            Assert.AreEqual(TaiyakiContentEnum.あんこ, miniTaiyaki.Content);
+            Assert.AreEqual(TaiyakiContentEnum.あんこ, middleTaiyaki.Content);
+            Assert.AreEqual(TaiyakiContentEnum.あんこ, bigTaiyaki.Content);
+
+            Assert.AreEqual(100, miniTaiyaki.GetSubTotal());
+            Assert.AreEqual(150, middleTaiyaki.GetSubTotal());
+            Assert.AreEqual(200, bigTaiyaki.GetSubTotal());
+        }
+    }
+}
---
+++ TaiyakiSystemTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TaiyakiSystemTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaiyakiSystemTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9773b4f6-fe9c-4368-8444-20744e22fd26")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

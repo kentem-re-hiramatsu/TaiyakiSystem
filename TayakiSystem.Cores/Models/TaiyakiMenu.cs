b7c773aef0cd990df3f69769c18ebe320085d449
re-hiramatsu <re-hiramatsu>
2024-07-01 14:28:54 +0900

    taiyakiMenuモデル実装

---
+++ TayakiSystem.Cores/Models/TaiyakiMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TaiyakiSystem.Cores.Enums;
+
+namespace TaiyakiSystem.Cores.Models
+{
+    public class TaiyakiMenu
+    {
+        private List<BaseTaiyaki> _taiyakiMenuList = new List<BaseTaiyaki>
+        {
+            new DefaultTaiyaki(),
+            new CustardTaiyaki(),
+            new DeluxeTaiyaki()
+        };
+
+        public BaseTaiyaki GetTaiyaki(int index, TaiyakiSizeEnum size)
+        {
+            return _taiyakiMenuList[index].GetCloneTaiyaki(size);
+        }
+    }
+}

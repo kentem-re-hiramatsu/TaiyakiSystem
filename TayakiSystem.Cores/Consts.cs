eab98005adf8e159cf203534f2092b0ae44bf15d
re-hiramatsu <re-hiramatsu>
2024-07-01 16:31:13 +0900

    たい焼きのインデックスを追加

--- TayakiSystem.Cores/Consts.cs
+++ TayakiSystem.Cores/Consts.cs
@@ -3,5 +3,8 @@
     public class Consts
     {
         public static readonly string BIG_SIZE_ONLY_ERROR_MESSAGE = "デラックスたい焼きは、'大'しか選択できません。";
+        public static readonly int DEFAULT_TAIYAKI_INDEX = 0;
+        public static readonly int CUSTARD_TAIYAKI_INDEX = 1;
+        public static readonly int DELUXE_TAIYAKI_INDEX = 2;
     }
 }

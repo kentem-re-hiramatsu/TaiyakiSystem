e6335f92d0745164e66d977419eb7279bfdc112c
re-hiramatsu <re-hiramatsu>
2024-07-01 14:30:02 +0900

    taiyakiMenu修正

---
+++ TaiyakiMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TaiyakiSystem.Cores.Models
+{
+    public class TaiyakiMenu
+    {
+        private List<BaseTaiyaki> _taiyakiMenuList = new List<BaseTaiyaki>();
+
+        public IReadOnlyCollection<BaseTaiyaki> TaiyakiMenuList { get { return _taiyakiMenuList; } }
+
+        public BaseTaiyaki GetTaiyaki(int index)
+        {
+            return _taiyakiMenuList[index];
+        }
+    }
+}

b0652f7c10c553fc600c5f888dcd78ee759e6a56
re-hiramatsu <re-hiramatsu>
2024-07-03 10:57:10 +0900

    Sizeの型をEnumからSizeクラスの型に変更に伴いGetSizePriceメソッド削除

--- TayakiSystem.Cores/Models/BaseTaiyaki.cs
+++ TayakiSystem.Cores/Models/BaseTaiyaki.cs
@@ -1,4 +1,5 @@
 ﻿using TaiyakiSystem.Cores.Enums;
+using TayakiSystem.Cores.Models;
 
 namespace TaiyakiSystem.Cores.Models
 {
@@ -6,24 +7,9 @@ namespace TaiyakiSystem.Cores.Models
     {
         public TaiyakiType Name { get; protected set; }
         public int TaiyakiPrice { get; protected set; }
-        public TaiyakiSizeType Size { get; set; }
-        public int SizePrice { get; protected set; }
+        public Size Size { get; set; }
         public string Content { get; protected set; }
 
-        public abstract BaseTaiyaki GetCloneTaiyaki(TaiyakiSizeType size);
         public abstract int GetSubTotal();
-        public int GetSizePrice(TaiyakiSizeType size)
-        {
-            switch (size)
-            {
-                case TaiyakiSizeType.小:
-                    return (int)TaiyakiSizeType.小;
-                case TaiyakiSizeType.中:
-                    return (int)TaiyakiSizeType.中;
-                case TaiyakiSizeType.大:
-                    return (int)TaiyakiSizeType.大;
-            }
-            return -1;
-        }
     }
 }

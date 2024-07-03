df11b147b2f758fa0917d86c2e82fbda2098c77e
re-hiramatsu <re-hiramatsu>
2024-07-03 09:53:10 +0900

    BaseTaiyaki.csのNameとContentの方をEnumからstringに変更 メンバ変数にSizePriceとTaiyakiPriceを追加

--- TayakiSystem.Cores/Models/BaseTaiyaki.cs
+++ TayakiSystem.Cores/Models/BaseTaiyaki.cs
@@ -4,11 +4,26 @@ namespace TaiyakiSystem.Cores.Models
 {
     public abstract class BaseTaiyaki
     {
-        public TaiyakiEnum Name { get; protected set; }
+        public string Name { get; protected set; }
+        public int TaiyakiPrice { get; protected set; }
         public TaiyakiSizeEnum Size { get; set; }
-        public TaiyakiContentEnum Content { get; protected set; }
+        public int SizePrice { get; protected set; }
+        public string Content { get; protected set; }
 
         public abstract BaseTaiyaki GetCloneTaiyaki(TaiyakiSizeEnum size);
         public abstract int GetSubTotal();
+        public int GetSizePrice(TaiyakiSizeEnum size)
+        {
+            switch (size)
+            {
+                case TaiyakiSizeEnum.小:
+                    return (int)TaiyakiSizeEnum.小;
+                case TaiyakiSizeEnum.中:
+                    return (int)TaiyakiSizeEnum.中;
+                case TaiyakiSizeEnum.大:
+                    return (int)TaiyakiSizeEnum.大;
+            }
+            return -1;
+        }
     }
 }
